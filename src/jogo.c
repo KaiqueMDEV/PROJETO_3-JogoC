627ca76fe00d922938d4c276ee3a49f1cc6b25f6
KaiqueMDEV <161653647+KaiqueMDEV@users.noreply.github.com>
2025-05-04 18:17:37 +0000

    comentários adicionados no código

--- src/jogo.c
+++ src/jogo.c
@@ -6,17 +6,17 @@
 #include <time.h>
 
 
-int intro(){
+int intro(){ //introdução do jogo, mostra um jogo simples de "fachada" e logo em seguida ocorre uma pequena interação com o "hacker".
     int escolha;
-    for(int i = 0; i < 3; i++){
+    for(int i = 0; i < 3; i++){ //repete o jogo falso por 3 rodadas ou até o jogador decidir parar.
         guessgame();
         printf("\nDeseja jogar novamente?\n1. Sim\n2. Não\n");
         scanf("%d", &escolha);
         if(escolha == 2){
-            break;
+            break; 
         }
-    }
-    digitacao();
+    } //inicio da interação com o hacker
+    digitacao(); 
     printf("anon1mo333: Está se divertindo? Vejo que gostou do meu joguinho, mas agora eu quero jogar também :) \n");
     fflush(stdout);
     sleep(3);
@@ -32,15 +32,15 @@ int intro(){
     printf("anon1mo333: Se você perder? Digamos apenas que... Você receberá uma visitinha\n");
     fflush(stdout);
     sleep(3);
-    printf("\nPressione ENTER para continuar para o menu principal.");
-    getchar();
-    getchar();
-    FILE *arquivo = fopen("log", "w"); //cria o log de ações do jogador após a cutscene.
+    printf("\nPressione ENTER para continuar para o menu principal."); //adiciona mais interatividade ao código
+    getchar(); //limpa o buffer
+    getchar(); //solicita uma entrada do usuário
+    FILE *arquivo = fopen("log", "w"); //cria o log de ações do jogador após a cena.
     fclose(arquivo);
 }
 
 void menu(){
-    limpartela();
+    limpartela(); 
     printf("\n----Menu Principal----\n");
     printf("Digite o que deseja fazer a seguir: \n1. Continuar História\n2. Reiniciar Save\n3. Sair\n");
     int escolha;
@@ -52,7 +52,7 @@ void menu(){
        break;
     case 2:
        printf("\nReiniciando Save...\n");
-       remove("log");
+       remove("log"); //remove o arquivo "log", futuramente será usado para apagar o arquivo de save do jogador.
        printf("LOG removido!");
        break;
     case 3:
@@ -63,29 +63,29 @@ void menu(){
     }
 };
 
-void digitacao(){
-    printf("anon1mo333: "); // print fixo
-    fflush(stdout);
+void digitacao(){ //função que simula uma interação com uma pessoa real, exibe no terminal uma digitação falsa que serve para dar mais imersão na cena
+    printf("anon1mo333: "); //print fixo
+    fflush(stdout); //limpa o 
 
-    for(int i = 0; i < 2; i++){ // simula por alguns ciclos
-        printf("\ranon1mo333: Digitando.  ");
+    for(int i = 0; i < 2; i++){ //simula por alguns ciclos
+        printf("\ranon1mo333: Digitando.  "); //esta parte atua como uma "animação", dando a entender que alguém está digitando de fato. 
+        fflush(stdout); //força a impressão para o terminal imediatamente.
+        sleep(1); //aguarda N segundos
+        printf("\ranon1mo333: Digitando.. "); //o /r força o cursor a voltar para o começo da linha antes de exibir a saída.
         fflush(stdout);
         sleep(1);
-        printf("\ranon1mo333: Digitando.. ");
-        fflush(stdout);
-        sleep(1);
-        printf("\ranon1mo333: Digitando...");
+        printf("\ranon1mo333: Digitando..."); 
         fflush(stdout);
         sleep(1);
     }
-    printf("\r                         \r");
+    printf("\r                         \r"); //este trecho serve para apagar qualquer coisa que esteja na linha antes da próxima mensagem.
     fflush(stdout);
 }
 
-int guessgame(){
+int guessgame(){ //jogo de fachada, um simples jogo de adivinhação, digite um número de 1 a 100 e o programa te indicará se é maior ou menor que o número correto
     srand(time(NULL));
     int numero;
-    int numero_correto = 69; //rand() % 101;
+    int numero_correto = 69; //rand() % 101; //gera um número aleatóriamente como resultado correto
     int escolha;
     printf("Olá! Bem vindo ao Guess Game! Adivinhe qual numero estou pensando de 1 a 100 :D\n");
     while(1){
--- src/main.c
+++ src/main.c
@@ -8,12 +8,12 @@
 
 
 
-int main(){
-    FILE *arquivo = fopen("log", "r");
-    if(arquivo == NULL){
+int main(){ 
+    FILE *arquivo = fopen("log", "r"); //abre o arquivo  "log" em modo de leitura para verificar se o mesmo existe
+    if(arquivo == NULL){ //se não existir, toca a introdução e ao final da mesma, cria o arquivo log.
         intro();
         menu();
-    }else{
+    }else{ //se existir, pula direto para o menu, dispensando a introdução.
         menu();
     }
 }
